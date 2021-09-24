58b703ae2599dfadc9a9a1c9bb441211d21902a6
dmitry <dmitry.sent@gmail.com>
2021-09-24 12:16:22 +0300

    finish game

--- index.js
+++ index.js
@@ -1,18 +1,35 @@
 let $start = document.querySelector('#start')
 let $game = document.querySelector('#game')
 let $time = document.querySelector('#time')
+let $timeHeader = document.querySelector('#time-header')
+let $resultHeader = document.querySelector('#result-header')
+let $result = document.querySelector('#result')
+let $gameTime = document.querySelector('#game-time')
 
+let colors = ['red', 'blue', 'green', 'pink']
 let score = 0
 let isGameStarted = false
 
 $start.addEventListener('click', startGame)
 $game.addEventListener('click', handleBoxClick)
+$gameTime.addEventListener('input', setGameTime)
 
+function show($el) {
+    $el.classList.remove('hide')
+}
+
+function hide($el) {
+    $el.classList.add('hide')
+}
 
 
 function startGame() {
+    score = 0
+    setGameTime()
+    $gameTime.setAttribute('disabled', true)
+
     isGameStarted = true
-    $start.classList.add('hide')
+    hide($start)
     $game.style.backgroundColor = '#fff'
 
     let interval = setInterval(function() {
@@ -29,8 +46,26 @@ function startGame() {
     renderBox()
 }
 
+function setGameScore() {
+    $result.textContent = score.toString()
+}
+
+function setGameTime() {
+    let time = +$gameTime.value
+    $time.textContent = time.toFixed(1)
+    show($timeHeader) 
+    hide($resultHeader)
+}
+
 function endGame() {
     isGameStarted = false
+    setGameScore()
+    $gameTime.removeAttribute('disabled')
+    show($start)
+    $game.style.backgroundColor = "#ccc"
+    $game.innerHTML = ''
+    hide($timeHeader)
+    show($resultHeader)
 }
 
 function handleBoxClick(event) {
@@ -51,10 +86,11 @@ function renderBox() {
     let gameSize = $game.getBoundingClientRect()
     let maxTop = gameSize.height - boxSize
     let maxLeft = gameSize.width - boxSize
+    let randomColorIndex = getRandom(0, colors.length)
 
     box.style.height = box.style.width = boxSize + 'px'
     box.style.position = 'absolute'
-    box.style.backgroundColor = '#000'
+    box.style.backgroundColor = colors[randomColorIndex]
     box.style.top = getRandom(0, maxTop) + 'px'
     box.style.left = getRandom(0, maxLeft) + 'px'
     box.style.cursor = 'pointer'
@@ -66,3 +102,4 @@ function renderBox() {
 function getRandom(min, max) {
     return Math.floor(Math.random() * (max - min) + min)
 }
+
